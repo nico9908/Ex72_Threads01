81b267f73fe7ba10fc3359eb738495d3cfe1a777
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2019-04-08 09:26:32 +0200

    implementeret 3 tråde

--- Ex72_Threads01/Program.cs
+++ Ex72_Threads01/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ex72_Threads01
@@ -10,6 +11,34 @@ namespace Ex72_Threads01
     {
         static void Main(string[] args)
         {
+            Thread t = new Thread(WriteHello);
+            Thread t2 = new Thread(WriteHello2);
+            Thread t3 = new Thread(WriteHello3);
+            t.Start();
+            t2.Start();
+            t3.Start();
+        }
+        static void WriteHello()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+        static void WriteHello2()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+        static void WriteHello3()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            Console.ReadKey();
         }
     }
 }
